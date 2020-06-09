3eb463cc464539dfc14ccfce4f1d06b68fe5f916
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-09 22:09:16 +0530

    feat: commented out global error handler

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
-Vue.config.errorHandler = function(err, vm, info) {
-  // handle error
-  console.log('GLOBAL ERROR HANDLER:', err, vm, info);
-  // `info` is a Vue-specific error info, e.g. which lifecycle hook
-  // the error was found in. Only available in 2.2.0+
-};
+// Vue.config.errorHandler = function(err, vm, info) {
+//   // handle error
+//   console.log('GLOBAL ERROR HANDLER:', err, vm, info);
+//   // `info` is a Vue-specific error info, e.g. which lifecycle hook
+//   // the error was found in. Only available in 2.2.0+
+// };
 
 new Vue({
   router,
