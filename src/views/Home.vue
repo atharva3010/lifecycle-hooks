c52320377dabaf85a8dab33d7cf418006a735e1d
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-09 10:38:20 +0530

    feat: added todo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,15 +10,20 @@
     </keep-alive>
     <input type="text" v-model="msg" />
     <button @click="resetMsg">Reset</button>
+    <br />
     <button @click="throwErr('error from button!')">
       Click to throw error!
     </button>
+    <!-- <button @click="fetchTodos">Fetch todos</button>
+    <div class="todo" v-for="todo in todos" :key="todo.id">
+      {{ todo.title }}
+    </div> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import TextDisplay from '@/components/TextDisplay.vue';
+import { httpClient } from '@/services/httpClient.js';
 
 export default {
   name: 'Home',
@@ -27,19 +32,23 @@ export default {
   },
   data() {
     return {
-      msg: 'Lifecycle Hooks in Vue!'
+      msg: 'Lifecycle Hooks in Vue!',
+      todos: []
     };
   },
 
   beforeCreate() {
     console.log('I am yet to be created, state is', this.msg);
   },
+
   created() {
     console.log('I am created, state is now', this.$data, 'DOM is', this.$el);
   },
+
   beforeMount() {
     console.log('I am yet to be mounted, DOM is', this.$el);
   },
+
   mounted() {
     console.log('I am mounted, DOM is', this.$el);
     this.$nextTick(function() {
@@ -47,25 +56,45 @@ export default {
       // throw new Error('I was born to error on mounted');
     });
   },
+
   beforeUpdate() {
     console.log('I am yet to be updated');
   },
+
   updated() {
     console.log('I have updated myself!');
   },
+
   beforeDestroy() {
     console.log('I am yet to be destroyed.');
     this.resetMsg();
   },
+
   destroyed() {
     console.log('Its over! RIP');
   },
+
   errorCaptured(err, component, details) {
     console.error('From errorCaptured:', err, component, details);
     return false;
   },
 
   methods: {
+    fetchTodos() {
+      return new Promise((resolve, reject) => {
+        httpClient.get(`todos`).then(
+          response => {
+            this.todos = response.data;
+            this.loading = false;
+            resolve(response.data);
+          },
+          error => {
+            this.loading = false;
+            reject(error);
+          }
+        );
+      });
+    },
     throwErr(errMsg) {
       throw new Error(errMsg);
       // This error will not be handled via the errorCaptured hook.
@@ -83,6 +112,11 @@ export default {
 
 <style scoped>
 button {
+  margin: 8px;
+}
+.todo {
+  height: 30px;
+  border: 1px gray solid;
   margin: 8px;
 }
 </style>
