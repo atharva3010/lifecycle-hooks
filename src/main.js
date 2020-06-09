a56951365eba8622c34c1e448170813b8c612c85
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-09 22:01:40 +0530

    feat: added vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 
@@ -14,5 +15,6 @@ Vue.config.errorHandler = function(err, vm, info) {
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app');
