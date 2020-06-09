0cca1d1400d80760ff0c89a1f02b6a3f10f821c5
Atharva Sharma <mailatharvasharma@gmail.com>
2020-06-09 09:47:12 +0530

    feat: added global errorHandler

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import router from './router';
 
 Vue.config.productionTip = false;
 
+Vue.config.errorHandler = function(err, vm, info) {
+  // handle error
+  console.error('From global errorHandler:', err, vm, info);
+  // `info` is a Vue-specific error info, e.g. which lifecycle hook
+  // the error was found in. Only available in 2.2.0+
+};
+
 new Vue({
   router,
   render: h => h(App)
